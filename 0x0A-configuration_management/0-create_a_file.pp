fd54f42fcc39d9b165e2a114bb636ed5fef0df47
KiPTM <mathewtuwei@gmail.com>
2024-04-22 05:42:53 -0700

    Update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,13 @@
-file { '/tmp/school':
-  ensure  => file,
+# Puppet manifest to create a file in /tmp
+
+# Define file path
+$file_path = '/tmp/school'
+
+# Ensure the file is present with the specified content, permissions, owner, and group
+file { $file_path:
+  ensure  => present,
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
 }
