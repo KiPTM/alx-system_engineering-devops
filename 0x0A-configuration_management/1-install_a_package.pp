cc46beccadc37d352fbc1bae39053c285343b52f
KiPTM <mathewtuwei@gmail.com>
2024-04-22 06:06:48 -0700

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
-# Puppet manifest to install Flask version 2.1.0 using pip3
+# Puppet manifest to install Flask version 2.1.0 using pip3.
 
 # Install Flask using pip3 with the specified version
 package { 'Flask':
