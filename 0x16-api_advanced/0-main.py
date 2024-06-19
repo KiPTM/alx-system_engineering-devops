1e56bb664a2339d7e6890dd9fc1dbcbbbaaa5c9e
KiPTM <mathewtuwei@gmail.com>
2024-06-19 06:34:20 +0000

    Update

--- 0x16-api_advanced/0-main.py
+++ 0x16-api_advanced/0-main.py
@@ -6,13 +6,18 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov)"
+        "User-Agent": "MathewTuweiBot/1.0 (by /u/MathewTuwei)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return None
-    results = response.json().get("data")
-    return results.get("subscribers", 0)
+    
+    if response.status_code == 200:
+        return 0
+    
+    data = response.json().get("data")
+    if data is None:
+        return 0
+    
+    return data.get("subscribers", 0)
 
 if __name__ == "__main__":
     existing_subreddit = "learnpython"
@@ -21,13 +26,12 @@ if __name__ == "__main__":
     existing_subscribers = number_of_subscribers(existing_subreddit)
     nonexisting_subscribers = number_of_subscribers(nonexisting_subreddit)
     
-    if existing_subscribers is not None:
+    if existing_subscribers != 0:
         print("OK")
     else:
         print("FAIL")
-
-    if nonexisting_subscribers is None:
+    
+    if nonexisting_subscribers == 0:
         print("OK")
     else:
         print("FAIL")
-
